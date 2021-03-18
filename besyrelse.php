5b894c65093fdb9a7c8fc146c8ebeaf0ebbabef2
oliver <olivermarco97@gmail.com>
2021-03-18 12:45:43 +0100

    midlertidig billede til bestyrelsesmedlemmer er blevet tilføjet

--- besyrelse.php
+++ besyrelse.php
@@ -18,6 +18,7 @@
     <meta name="copyright" content="Information om copyright">
 
     <!-- Sikrer man kan benytte CSS ved at tilkoble en CSS fil -->
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
     <link href="css/styles.css" rel="stylesheet" type="text/css">
 
     <!-- Sikrer den vises korrekt på mobil, tablet mv. ved at tage ift. skærmstørrelse - bliver brugt til responsive websider -->
@@ -26,8 +27,15 @@
 
 <!-- i <body> har man alt indhold på siden som brugeren kan se -->
 <body>
-
+<div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
 <!-- Her skal sidens indhold ligge -->
-
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
